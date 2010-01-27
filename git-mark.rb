fd37ade0fc46a8ed7daf4c507cd532085988441b
Etienne Vallette d'Osia <Etienne.Vallette-dOsia@mobile-devices.fr>
2010-01-27 10:42:55 +0100

    add options select, replacing exclude

--- git-mark.rb
+++ git-mark.rb
@@ -26,6 +26,7 @@ end
 
 opts = {
   :patterns => [],
+  :selects => [],
   :excludes => ['refs/remotes/'],
   :add => [],
   :delete => [],
@@ -39,14 +40,11 @@ while arg = args.shift
     opts[:file] = next_arg(arg, args)
 
   when /^-r|--remote$/
-    opts[:excludes].delete('refs/remotes/')
-    opts[:excludes] << 'refs/heads/'
+    opts[:selects] << 'refs/remotes/'
   when /^-A|--All$/
-    opts[:excludes].delete('refs/heads/')
-    opts[:excludes].delete('refs/remotes/')
+    opts[:selects] << 'refs/(heads|remotes)/'
   when /^-a|--all$/
-    opts[:excludes].delete('refs/heads/')
-    opts[:excludes].delete('refs/remotes/')
+    opts[:selects] << '*'
   when /^-e|--exclude/
     opts[:excludes] << next_arg(arg, args)
 
@@ -71,10 +69,14 @@ end
 opts[:file] ||= ENV['GIT_MARKS_FILE'] || %x(git config --get marks.file || echo $(git rev-parse --git-dir)/marks)
 opts[:file].sub!(/\r?\n?$/,'')
 
+opts[:selects] << 'refs/heads/' if opts[:selects].empty?
+opts[:selects].map!{|p| glob_to_reg(p) }
 refs = {}
+show = []
 %x(git show-ref --abbrev).each_line do |line|
   hash, ref = line.split(' ')
   refs[ref] = [hash, []]
+  show << ref if opts[:selects].any?{|p| ref =~ p }
 end
 
 if File.exists? opts[:file]
@@ -90,49 +92,44 @@ if File.exists? opts[:file]
   end
 end
 
-show = []
 if opts[:list].empty?
   opts[:patterns].map!{|p| glob_to_reg(p) }
   opts[:excludes].map!{|p| glob_to_reg(p) }
-  refs.each do |ref, (hash,marks)|
+  show.map! do |ref|
     case
     when opts[:excludes].any?{|p| ref =~ p }
-      add = false
     when opts[:patterns].any?{|p| ref =~ p }
-      add = true
-    else
-      add = false
+      ref
     end
-    show << ref if add
   end
+  show.compact!
 
   opts[:add].each do |add|
     add = add.split(/\s*,\s*/)
-    show.each do |ref, short_ref|
+    show.each do |ref|
       refs[ref][1].concat(add)
     end
   end
 
   opts[:delete].each do |delete|
     delete = delete.split(/\s*,\s*/)
-    show.each do |ref, short_ref|
+    show.each do |ref|
       delete.each{|d| refs[ref][1].delete(d) }
     end
   end
 else
   opts[:list].map!{|p| glob_to_reg(p) }
   opts[:exclude_list].map!{|p| glob_to_reg(p) }
-  refs.each do |ref, (hash,marks)|
+  show.map! do |ref|
+    head, marks = refs[ref]
     case
     when opts[:exclude_list].any?{|p| marks.any?{|m| m =~ p } }
-      add = false
     when opts[:list].any?{|p| marks.any?{|m| m =~ p } }
-      add = true
+      ref
     else
-      add = false
     end
-    show << ref if add
   end
+  show.compact!
 end
 show = show.inject({}) do |res, ref|
   res[ref] = ref.sub(%r[^refs/(heads/|remotes/)?],'')
